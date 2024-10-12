4c657a450922751491ada56649eb89c1cdd48da2
Taomosquito <S_Peloquin123@live.com>
2024-10-12 01:06:27 -0400

    update to benchmark method

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,11 +1,16 @@
-def benchmark
-  # Your benchmarking code goes here.
+def benchmark (value)
+  start_time = Time.now
+  puts value
+  end_time = Time.now
+
+  # This will return the difference in the timestamps in seconds
+  running_time = end_time - start_time
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
 # It's a loooong string. :)
 long_string = "apple"*100000000
  
-running_time = benchmark { long_string.reverse }
+running_time = benchmark(long_string.reverse)
  
 puts "string.reverse took #{running_time} seconds to run"
